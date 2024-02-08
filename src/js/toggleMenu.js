7d206debb704c8ac30de2f18fc7e1c03a871c02b
Julia Grigoreva <julia_panina@mail.ru>
2024-02-08 23:41:46 +0300

    fix

--- src/js/toggleMenu.js
+++ src/js/toggleMenu.js
@@ -1,26 +1,26 @@
 const toggleMenu = document.querySelector('.menu-toggle');
-// const asideMenu = document.querySelector('.aside-menu-wrapper');
-// const bodyEl = document.body;
+const asideMenu = document.querySelector('.aside-menu-wrapper');
+const bodyEl = document.body;
 
 // for (let item of toggleMenu) {
     toggleMenu.addEventListener('click', function(){
        
         if(this.classList.contains('active')){
             this.classList.remove('active');
-            // asideMenu.classList.remove('active');
-            // bodyEl.classList.remove('noscroll');
+            asideMenu.classList.remove('active');
+            bodyEl.classList.remove('noscroll');
         }else{
             this.classList.add('active');
-            // asideMenu.classList.add('active');
-            // bodyEl.classList.add('noscroll');
+            asideMenu.classList.add('active');
+            bodyEl.classList.add('noscroll');
         }
     });
 // };
 
-// asideMenu.addEventListener('click', function (e) {
-//     if (e.target === e.currentTarget){
-//         asideMenu.classList.remove('active');
-//         bodyEl.classList.remove('noscroll');
-//         for (let item of toggleMenu) { item.classList.remove('active'); }
-//     }
-// });
+asideMenu.addEventListener('click', function (e) {
+    if (e.target === e.currentTarget){
+        asideMenu.classList.remove('active');
+        bodyEl.classList.remove('noscroll');
+        toggleMenu.classList.remove('active'); }
+    }
+);
