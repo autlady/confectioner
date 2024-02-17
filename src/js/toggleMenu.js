d2f7452a3bcbc1ec877c60175c9963204297cd85
Julia Grigoreva <julia_panina@mail.ru>
2024-02-17 21:56:37 +0300

    fix

--- src/js/toggleMenu.js
+++ src/js/toggleMenu.js
@@ -2,25 +2,25 @@ const toggleMenu = document.querySelector('.menu-toggle');
 const asideMenu = document.querySelector('.aside-menu-wrapper');
 const bodyEl = document.body;
 
-// for (let item of toggleMenu) {
-    toggleMenu.addEventListener('click', function(){
-       
-        if(this.classList.contains('active')){
-            this.classList.remove('active');
-            asideMenu.classList.remove('active');
-            bodyEl.classList.remove('noscroll');
-        }else{
-            this.classList.add('active');
-            asideMenu.classList.add('active');
-            bodyEl.classList.add('noscroll');
-        }
-    });
-// };
 
-asideMenu.addEventListener('click', function (e) {
-    if (e.target === e.currentTarget){
+toggleMenu.addEventListener('click', function () {
+
+    if (this.classList.contains('active')) {
+        this.classList.remove('active');
         asideMenu.classList.remove('active');
         bodyEl.classList.remove('noscroll');
-        toggleMenu.classList.remove('active'); }
+    } else {
+        this.classList.add('active');
+        asideMenu.classList.add('active');
+        bodyEl.classList.add('noscroll');
     }
+});
+
+asideMenu.addEventListener('click', function () {
+
+    asideMenu.classList.remove('active');
+    bodyEl.classList.remove('noscroll');
+    toggleMenu.classList.remove('active');
+
+}
 );
